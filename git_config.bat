632131c832641416a132c39a79aff09bdceffbc8
cr.cardd <148579191+CrCardd@users.noreply.github.com>
2024-06-20 09:33:52 -0300

    Update git_config.bat

--- git_config.bat
+++ git_config.bat
@@ -5,4 +5,4 @@ git config --global http.proxy http://disrct:etstech31415@rb-proxy-ca1.bosch.com
 git config --global https.proxy https://disrct:etstech31415@rb-proxy-ca1.bosch.com:8080
 
 git config --global user.name "Dudous"
-git config --global user.email "sampaioeduardo36@gmail.com"
+git config --global user.email "sampaioeeduardo36@gmail.com"
