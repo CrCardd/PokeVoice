ff591b1f7d43734e6da2891fcd1f7f7dda11b631
matiaszo <tiaszuniga@gmail.com>
2024-06-27 16:26:12 -0300

    Arrumando arquivos

--- maps/buildMap.h
+++ maps/buildMap.h
@@ -20,55 +20,27 @@ void showMap(Map *map)
                     printf("   ");
                 else
                     printf("// ");   
-                       
+
             printf("\n");  
         }
 }
 
 
-void buildLine(int mode,Map *map,int coord_x, int coord_y, int size)
+void buildLine(int mode,Map *map,int coordX, int coordY, int size)
 {
     if(mode == 1)
     {
-        for (int i = coord_x; i < coord_x + size; i++)
-            map->map[coord_y][i] = 1;
+        for (int i = coordX; i < coordX + size; i++)
+            map->map[coordY][i] = 1;
         
     }else if (mode == 2)
     {
-        for (int i = coord_y; i < coord_y + size; i++)
-            map->map[i][coord_x] = 1;
+        for (int i = coordY; i < coordY + size; i++)
+            map->map[i][coordX] = 1;
     }else
     {
-        map->map[coord_x][coord_y] = 5;
+        map->map[coordX][coordY] = 5;
     }    
-}
-
-
-void buildSquare(int mode, Map *map, int coordUpY, int coordUpX, int coordDownY, int coordDownX)
-{
-    int lenght = coordDownX - coordUpX;
-    int height = coordDownY - coordUpY;
-    int biggest = (height>lenght)?height:lenght;
-    if (mode == 1)
-    {
-        for(float i=0; i<=biggest; i++)
-        {
-            map->map[coordUpY + (int)((i/biggest)*height)][coordUpX] = 1;
-            map->map[coordDownY - (int)((i/biggest)*height)][coordDownX] = 1;
-            map->map[coordUpY][coordUpX + (int)((i/biggest)*lenght)] = 1;
-            map->map[coordDownY][coordDownX - (int)((i/biggest)*lenght)] = 1;
-        }
-    }else if (mode == 2)
-    {
-        for(float i=0; i<=biggest; i++)
-        {
-            map->map[coordUpY + (int)((i/biggest)*height)][coordUpX] = 1;
-            map->map[coordDownY - (int)((i/biggest)*height)][coordDownX] = 1;
-            map->map[coordUpY][coordUpX + (int)((i/biggest)*lenght)] = 1;
-            printf("xx yy");
-        }
-    }else
-        map->map[coordDownY][coordDownX] = 5;
 }
 
 #endif 
--- maps/map1.c
+++ maps/map1.c
@@ -3,6 +3,7 @@
 
 int main()
 {
+system("color 50");
 Player player = playerInnit(0,0);
 Map map = gameInnit(63,79, player);
 
@@ -11,19 +12,18 @@ buildLine(1,&map, 0, 62, 79);
 buildLine(2,&map, 0, 0, 62);
 buildLine(2,&map, 78, 0, 62);
 
-// buildSquare(1, &map, 0, 0, 24, 36);
 
 buildLine(2,&map, 29, 0, 24);
-
-buildLine(1, &map, 0, 24, 78);
 buildLine(2, &map, 58, 0, 24);
 
-buildLine(2, &map, 20, 24, 33);
-buildLine(1, &map, 20, 54, 15);
-buildLine(2, &map, 35, 24, 38);
+buildLine(1, &map, 0, 24, 78);
 
-buildLine(1, &map, 50, 45, 28);
+buildLine(2, &map, 20, 24, 33);
+buildLine(2, &map, 35, 24, 38);
 buildLine(2, &map, 50, 24, 26);
+
+buildLine(1, &map, 20, 54, 15);
+buildLine(1, &map, 50, 45, 28);
 
 
 showMap(&map);
