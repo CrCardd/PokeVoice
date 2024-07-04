3ace3a8771e8ef4fdcb303ea41096131fe347041
matiaszo <tiaszuniga@gmail.com>
2024-07-04 02:42:59 -0300

    Frases feitas da tela de interacao

--- headers/buildMap.h
+++ headers/buildMap.h
@@ -85,10 +85,10 @@ void fightScreenConstruct(Map **screen)
     buildLine(2,screen,78,36,26);
 
 
-    buildLine(2,screen,2,37,24);
-    buildLine(2,screen,31,37,24);
-    buildLine(1,screen,2,37,29);
-    buildLine(1,screen,2,60,29);
+    // buildLine(2,screen,2,37,24);
+    // buildLine(2,screen,31,37,24); 
+    // buildLine(1,screen,2,37,29);
+    // buildLine(1,screen,2,60,29);
 
 
 
@@ -102,6 +102,66 @@ void fightScreenConstruct(Map **screen)
     //QUADRANTES
         //1/4
             // buildSquare(screen,37,33,48,54,1);
+    //WHAT
+            buildLine(2,screen, 2, 39, 8);
+            buildLine(1,screen, 2, 46, 2);
+            buildLine(2,screen, 4, 43, 4);
+            buildLine(1,screen, 4, 46, 2);
+            buildLine(2,screen, 6, 39, 8);
+
+            buildLine(2,screen, 8, 39, 8);
+            buildLine(1,screen, 8, 42, 2);
+            buildLine(2,screen, 10, 39, 8);
+
+            buildLine(2,screen, 12, 39, 8);
+            buildLine(1,screen, 12, 39, 2);
+            buildLine(2,screen, 14, 39, 8);
+            buildLine(1,screen, 12, 42, 2);
+            
+            buildLine(1,screen, 16, 39, 3);
+            buildLine(2,screen, 17, 39, 8);
+
+    //WILL
+            buildLine(2,screen, 20, 39, 8);
+            buildLine(1,screen, 20, 46, 2);
+            buildLine(2,screen, 22, 43, 4);
+            buildLine(1,screen, 22, 46, 2);
+            buildLine(2,screen, 24, 39, 8);
+            
+            buildLine(2,screen, 26, 40, 7);
+
+            buildLine(2,screen, 28, 39, 8);
+            buildLine(1,screen, 28, 46, 2);
+            
+            buildLine(2,screen, 31, 39, 8);
+            buildLine(1,screen, 31, 46, 2);
+            
+    //DO
+            buildLine(2,screen, 20, 52, 7);
+            buildLine(1,screen, 20, 51, 2);
+            buildLine(2,screen, 22, 51, 1);
+            buildLine(1,screen, 22, 52, 1);
+            buildLine(2,screen, 23, 52, 7);
+            buildLine(1,screen, 22, 58, 1);
+            buildLine(2,screen, 22, 59, 1);
+            buildLine(1,screen, 20, 59, 2);
+
+            buildLine(2,screen, 27, 51, 8);
+            buildLine(2,screen, 25, 51, 8);
+            buildLine(1,screen, 25, 59, 3);
+            buildLine(1,screen, 25, 51, 2);
+
+            
+            buildLine(2,screen, 29, 50, 3);
+            buildLine(1,screen, 29, 50, 2);
+            buildLine(2,screen, 31, 50, 5);
+            buildLine(1,screen, 29, 54, 2);
+            buildLine(2,screen, 29, 54, 4);
+            buildLine(2,screen, 29, 60, 1);//Cristian arruma
+
+            
+
+
         //2/4
             // buildSquare(screen,37,55,48,76,1);
         //3/4
@@ -115,7 +175,7 @@ void fightScreenConstruct(Map **screen)
             buildLine(2,screen,35,40,7);
             buildLine(1,screen,36,43,1);
         //I
-            buildLine(2,screen,38,39,8);
+            buildLine(2,screen,38,40,7);
         //G
             buildLine(1,screen,40,39,4);
             buildLine(1,screen,41,46,4);
@@ -154,6 +214,44 @@ void fightScreenConstruct(Map **screen)
             buildLine(2,screen,71,52,7);
 
             buildLine(1,screen,72,58,2);
+
+    //BAG
+
+            buildLine(2,screen, 59, 40, 6);
+            buildLine(2,screen, 62, 40, 6);
+            buildLine(1,screen, 59, 46, 3);
+            buildLine(1,screen, 59, 39, 3);
+            buildLine(1,screen, 61, 43, 1);
+
+            buildLine(2,screen, 64, 39, 7);
+            buildLine(1,screen, 64, 39, 3);
+            buildLine(2,screen, 67, 39, 7);
+            buildLine(1,screen, 64, 42, 3);
+
+            buildLine(2,screen, 69, 39, 7);
+            buildLine(1,screen, 69, 39, 3);
+            buildLine(1,screen, 69, 46, 4);
+            buildLine(2,screen, 72, 44, 2);
+            buildLine(1,screen, 71, 43, 2);
+
+    //BALL
+
+            buildLine(2,screen, 36, 52, 6);
+            buildLine(2,screen, 39, 52, 6);
+            buildLine(1,screen, 36, 58, 3);
+            buildLine(1,screen, 36, 51, 3);
+            buildLine(1,screen, 38, 55, 1);
+
+            buildLine(2,screen, 41, 51, 7);
+            buildLine(1,screen, 41, 51, 3);
+            buildLine(2,screen, 44, 51, 7);
+            buildLine(1,screen, 41, 54, 3);
+
+            buildLine(2,screen, 46, 51, 7);
+            buildLine(1,screen, 46, 58, 3);
+
+            buildLine(2,screen, 50, 51, 7);
+            buildLine(1,screen, 50, 58, 3);
 
 
 }
